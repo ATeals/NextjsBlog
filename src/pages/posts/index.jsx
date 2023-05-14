5effe8f6793f2f145527335905c616bf7d844a55
ateals <ateals@icloud.com>
2023-05-14 23:13:28 +0900

    test

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -21,7 +21,12 @@ export default ({ collections, posts }) => {
 
 export const getStaticProps = async () => {
     //allPosts => 해당 경로의 mdx파일을 배열에 담아서 전송해줌!
-    const collections = getCollectionAll();
+    const collections = allPosts
+        .filter((i) => i._raw.sourceFilePath.includes("/index.mdx"))
+        .map((item) => ({
+            ...item,
+            posts: getPostAll().filter((i) => i._raw.sourceFilePath.includes(item._raw.flattenedPath)),
+        }));
     const posts = allPosts.filter((i) => !i._raw.sourceFilePath.includes("/index.mdx"));
     return {
         props: {
