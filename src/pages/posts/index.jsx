5dbe2235b90aa30766b6229645db58c6a697b743
ateals <ateals@icloud.com>
2023-05-14 18:57:57 +0900

    test-2

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -3,26 +3,26 @@ import CardList from "@/components/blog/blogList/CardList";
 import { useEffect, useState } from "react";
 
 export default ({ posts }) => {
-    const [allPost, setAllPost] = useState(allPosts);
+    // const [allPost, setAllPost] = useState(allPosts);
 
     return (
         <>
-            {/* <CardList posts={posts} /> */}
-            <section className="@apply flex justify-center flex-col">
+            <CardList posts={posts} />
+            {/* <section className="@apply flex justify-center flex-col">
                 <h1 className="text-center text-[2em]">--- All Post ---</h1>
                 <CardList posts={allPost} />
-            </section>
+            </section> */}
         </>
     );
 };
 
-// export const getStaticProps = async () => {
-//     //allPosts => 해당 경로의 mdx파일을 배열에 담아서 전송해줌!
-//     const posts = allPosts.filter((i) => i._raw.sourceFilePath.includes("/index.mdx"));
+export const getStaticProps = async () => {
+    //allPosts => 해당 경로의 mdx파일을 배열에 담아서 전송해줌!
+    const posts = allPosts.filter((i) => i._raw.sourceFilePath.includes("/index.mdx"));
 
-//     return {
-//         props: {
-//             posts,
-//         },
-//     };
-// };
+    return {
+        props: {
+            posts,
+        },
+    };
+};
