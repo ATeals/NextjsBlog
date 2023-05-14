cbec5f0011c5191ec0097d58c09674058a4bc6e2
ateals <ateals@icloud.com>
2023-05-14 18:56:23 +0900

    test

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -7,7 +7,7 @@ export default ({ posts }) => {
 
     return (
         <>
-            <CardList posts={posts} />
+            {/* <CardList posts={posts} /> */}
             <section className="@apply flex justify-center flex-col">
                 <h1 className="text-center text-[2em]">--- All Post ---</h1>
                 <CardList posts={allPost} />
@@ -16,13 +16,13 @@ export default ({ posts }) => {
     );
 };
 
-export const getStaticProps = async () => {
-    //allPosts => 해당 경로의 mdx파일을 배열에 담아서 전송해줌!
-    const posts = allPosts.filter((i) => i._raw.sourceFilePath.includes("/index.mdx"));
+// export const getStaticProps = async () => {
+//     //allPosts => 해당 경로의 mdx파일을 배열에 담아서 전송해줌!
+//     const posts = allPosts.filter((i) => i._raw.sourceFilePath.includes("/index.mdx"));
 
-    return {
-        props: {
-            posts,
-        },
-    };
-};
+//     return {
+//         props: {
+//             posts,
+//         },
+//     };
+// };
