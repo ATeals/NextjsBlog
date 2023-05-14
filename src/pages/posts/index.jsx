3f7b9063203dcc0891dc11477e900eef71f6f206
ateals <ateals@icloud.com>
2023-05-15 00:05:03 +0900

    test

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,7 +1,7 @@
 import { allPosts } from "contentlayer/gererated";
 import CardList from "@/components/blog/blogList/CardList";
 import LineList from "@/components/blog/blogList/LineList";
-import { getPostAll, getCollectionAll } from "../../lib/Postdata";
+import { getPostAll } from "../../lib/Postdata";
 
 export default ({ collections, posts }) => {
     return (
@@ -20,15 +20,13 @@ export default ({ collections, posts }) => {
 };
 
 export const getStaticProps = async () => {
+    //allPosts => 해당 경로의 mdx파일을 배열에 담아서 전송해줌!
     const collections = allPosts
         .filter((i) => i._raw.sourceFilePath.includes("/index.mdx"))
         .map((item) => ({
             ...item,
             posts: allPosts.filter((i) => i._raw.sourceFilePath.includes(item._raw.flattenedPath)).filter((i) => !i._raw.sourceFilePath.includes("/index.mdx")),
         }));
-
-    // const collections = getCollectionAll();
-
     const posts = getPostAll();
     return {
         props: {
