6dbc0ce34ebed60e937b4a9eb5f6eeaecdf9a62e
ateals <ateals@icloud.com>
2023-05-14 18:59:54 +0900

    test-3

--- src/pages/posts/index.jsx
+++ src/pages/posts/index.jsx
@@ -1,17 +1,14 @@
 import { allPosts } from "contentlayer/gererated";
 import CardList from "@/components/blog/blogList/CardList";
-import { useEffect, useState } from "react";
 
-export default ({ posts }) => {
-    // const [allPost, setAllPost] = useState(allPosts);
-
+export default ({ posts, allPost }) => {
     return (
         <>
             <CardList posts={posts} />
-            {/* <section className="@apply flex justify-center flex-col">
+            <section className="@apply flex justify-center flex-col">
                 <h1 className="text-center text-[2em]">--- All Post ---</h1>
                 <CardList posts={allPost} />
-            </section> */}
+            </section>
         </>
     );
 };
@@ -19,10 +16,11 @@ export default ({ posts }) => {
 export const getStaticProps = async () => {
     //allPosts => 해당 경로의 mdx파일을 배열에 담아서 전송해줌!
     const posts = allPosts.filter((i) => i._raw.sourceFilePath.includes("/index.mdx"));
-
+    const allPost = allPosts;
     return {
         props: {
             posts,
+            allPost,
         },
     };
 };
