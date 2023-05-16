beaa252aa43e212945f3209ec26830cbd56a87d3
ateals <ateals@icloud.com>
2023-05-16 19:10:53 +0900

    naver serch meta

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -31,6 +31,10 @@ const MyDocument = () => {
                     name="google-site-verification"
                     content="Vf0Y1AEjfuPI3lth_ZcM3F4TI0bW9UxJ_7ZJQm_pC4w"
                 />
+                <meta
+                    name="naver-site-verification"
+                    content="514746f4453f87d737d5f285ed615084022639fc"
+                />
             </Head>
             <body className="md:px-[10%] dark:bg-[#121212] dark:text-[#E0E0E0]">
                 <Main />
