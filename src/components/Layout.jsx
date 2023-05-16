7faaffb41461704fe12fd06350e1b0f3e2e446ff
ateals <ateals@icloud.com>
2023-05-16 19:51:19 +0900

    default Seo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,41 @@ import Nav from "@/components/Nav";
 import Head from "next/head";
 import Footer from "@/components/seo/Footer";
 
+import { DefaultSeo } from "next-seo";
+
+const DEFAULT_SEO = {
+    title: "Ateals Blog",
+    description: "Welcome my Blog!",
+    canonical: "https://www.carrotins.com",
+    openGraph: {
+        type: "website",
+        locale: "ko_KR",
+        url: "https://nextjs-blog-ateals.vercel.app/",
+        title: "Ateals Blog",
+        site_name: "Ateals Blog",
+        images: [
+            {
+                url: "https://nextjs-blog-ateals.vercel.app/images/logoImg.png",
+                width: 1900,
+                height: 630,
+                alt: "og: 이미지",
+            },
+        ],
+    },
+    twitter: {
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+    },
+};
+
 export default function Layout({ children }) {
     return (
         <>
             <Head>
                 <title>Ateal's Blog</title>
             </Head>
+            <DefaultSeo {...DEFAULT_SEO} />
             <div className="@apply min-h-screen relative flex flex-col">
                 <header>
                     <Nav />
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,23 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { useRef } from "react";
-
-// class MyDocument extends Document {
-//     render() {
-//         return (
-//             // html 태그에 언어 설정
-//             <Html
-//                 lang="ko"
-//                 className="dark"
-//             >
-//                 <Head />
-//                 <body className="md:px-[10%] dark:bg-[#121212] dark:text-[#E0E0E0]">
-//                     <Main />
-//                     <NextScript />
-//                 </body>
-//             </Html>
-//         );
-//     }
-// }
+import { Html, Head, Main, NextScript } from "next/document";
 
 const MyDocument = () => {
     return (
