358c5d0d408bdeef6a2b54b022b08a7f4c480c83
ateals <ateals@icloud.com>
2023-05-16 19:07:58 +0900

    google search

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -26,7 +26,12 @@ const MyDocument = () => {
             lang="ko"
             className=""
         >
-            <Head />
+            <Head>
+                <meta
+                    name="google-site-verification"
+                    content="Vf0Y1AEjfuPI3lth_ZcM3F4TI0bW9UxJ_7ZJQm_pC4w"
+                />
+            </Head>
             <body className="md:px-[10%] dark:bg-[#121212] dark:text-[#E0E0E0]">
                 <Main />
                 <NextScript />
