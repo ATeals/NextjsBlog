84826e72d6225673d4135fe60ed031c7369b592e
ateals <ateals@icloud.com>
2023-05-22 03:18:45 +0900

    toc 외부클릭시 사라짐

--- src/components/toc/TocMenu.jsx
+++ src/components/toc/TocMenu.jsx
@@ -1,31 +1,47 @@
-import { MenuOutlined } from "@ant-design/icons";
-import { useRef } from "react";
 import Toc from "@/components/toc/Toc";
+import { useRef, useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default ({ post }) => {
-    const menu = useRef(null);
+    const [onMenu, setOnMenu] = useState(false);
+    const menuRef = useRef(null);
 
-    const onClickHandler = () => {
-        menu.current.classList.toggle("hidden");
+    const clickHander = () => {
+        setOnMenu((i) => !i);
     };
+
+    const menuCloseHandler = ({ target }) => {
+        if (onMenu && !menuRef.current.contains(target)) setOnMenu(false);
+    };
+
+    useEffect(() => {
+        window.addEventListener("click", menuCloseHandler);
+        return () => {
+            window.removeEventListener("click", menuCloseHandler);
+        };
+    });
 
     return (
         <>
-            <div
-                onClick={onClickHandler}
-                className="md:hidden text-[#eeeff2] fixed bottom-0 right-0 m-[25px] w-[2em] h-[2em] bg-[#577cf1] rounded-[10px] flex items-center justify-center"
-            >
-                <MenuOutlined />
-            </div>
-            <div
-                ref={menu}
-                className={`dark:bg-[#1E1E1E] hidden z-[1] w-[70%] fixed bottom-0 right-0 m-[25px] bg-[#ffffff] rounded-[10px]`}
-            >
-                <Toc
-                    post={post}
-                    menuToggle={onClickHandler}
-                />
-            </div>
+            {onMenu ? (
+                <div
+                    ref={menuRef}
+                    className={`dark:bg-[#1E1E1E] z-[1] w-[70%] fixed bottom-0 right-0 m-[25px] bg-[#ffffff] rounded-[10px]`}
+                >
+                    <Toc
+                        post={post}
+                        menuToggle={clickHander}
+                    />
+                </div>
+            ) : (
+                <div
+                    onClick={clickHander}
+                    className=" cursor-pointer md:hidden z-[0] text-[#eeeff2] fixed bottom-0 right-0 m-[25px] w-[2.5em] h-[2.5em] bg-[#577cf1] rounded-[10px] flex items-center justify-center"
+                >
+                    📑
+                </div>
+            )}
         </>
     );
 };
