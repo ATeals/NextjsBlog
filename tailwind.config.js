5500dcaa1cb8f0cb0a65f32afa15d22dca3ff708
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-06-10 17:45:38 +0900

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: "class",
-    content: ["./src/**/*.{js,ts,jsx,tsx,mdx,css}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
