77238123bbca97386855fb3a04e1e67dbf522b1c
A_Teals <125727432+ATeals@users.noreply.github.com>
2023-06-10 17:44:44 +0900

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://nextjs-blog-ateals.vercel.app",
-    changefreq: "daily",
-    priority: 0.7,
-    sitemapSize: 7000,
-    generateRobotsTxt: true,
-    exclude: [],
-};
