23775e08c6fa2064fc3c5bf68116f2dc26b0931e
ateals <ateals@icloud.com>
2023-06-17 00:16:39 +0900

    console request

--- src/app/api/getOg/route.ts
+++ src/app/api/getOg/route.ts
@@ -15,13 +15,13 @@ const getOg = (url: string) =>
 export async function GET(request: NextRequest, response: NextResponse) {
     // const url = request.nextUrl.searchParams.get("url");
     // const { api_key } = JSON.parse(decodeURIComponent(request.headers.get("x-invoke-query") || ""));
-    const head = request.headers;
+    const head = request;
     console.log("data", head);
     // if (api_key !== process.env.API_KEY) return NextResponse.json({ title: "NOT MATCH API KEY", api_key });
     // if (!url) return NextResponse.json({ title: "NO URL" });
 
     // const data = await getOg(encodeURI(url));
-    return NextResponse.json({ header: String(head), query: String(head.get("x-invoke-query")) });
+    return NextResponse.json({ header: String(head) });
 }
 
 // export async function GET(request: NextRequest) {
