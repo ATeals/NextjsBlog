dbcfcfe1d108ce08eea8fe498d7638e5cca7c581
ateals <ateals@icloud.com>
2023-06-17 00:27:55 +0900

    next/header

--- src/app/api/getOg/route.ts
+++ src/app/api/getOg/route.ts
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import og from "open-graph";
 
@@ -15,8 +16,9 @@ const getOg = (url: string) =>
 export async function GET(request: NextRequest, response: NextResponse) {
     // const url = request.nextUrl.searchParams.get("url");
     // const { api_key } = JSON.parse(decodeURIComponent(request.headers.get("x-invoke-query") || ""));
+
     const head = request;
-    console.log("data", head);
+    console.log("Headers", headers());
     // if (api_key !== process.env.API_KEY) return NextResponse.json({ title: "NOT MATCH API KEY", api_key });
     // if (!url) return NextResponse.json({ title: "NO URL" });
 
