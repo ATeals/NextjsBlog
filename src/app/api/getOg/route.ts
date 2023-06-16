714d57657923ea8f27a54f521f009785c135afc5
ateals <ateals@icloud.com>
2023-06-16 23:43:14 +0900

    test2

--- src/app/api/getOg/route.ts
+++ src/app/api/getOg/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest, response: NextResponse) {
     // if (!url) return NextResponse.json({ title: "NO URL" });
 
     // const data = await getOg(encodeURI(url));
-    return NextResponse.json(data);
+    return NextResponse.json(request.headers);
 }
 
 // export async function GET(request: NextRequest) {
