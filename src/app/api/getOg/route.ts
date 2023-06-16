caa9e7d74aeff4ccf54ac80925399f6c97821438
ateals <ateals@icloud.com>
2023-06-16 23:34:11 +0900

    test

--- src/app/api/getOg/route.ts
+++ src/app/api/getOg/route.ts
@@ -13,15 +13,14 @@ const getOg = (url: string) =>
     });
 
 export async function GET(request: NextRequest, response: NextResponse) {
-    const url = request.nextUrl.searchParams.get("url");
-    console.log(decodeURIComponent(request.headers.get("x-invoke-query") || ""));
-    const { api_key } = JSON.parse(decodeURIComponent(request.headers.get("x-invoke-query") || ""));
+    // const url = request.nextUrl.searchParams.get("url");
+    // const { api_key } = JSON.parse(decodeURIComponent(request.headers.get("x-invoke-query") || ""));
+    const data = JSON.parse(decodeURIComponent(request.headers.get("x-invoke-query") || ""));
+    console.log(data);
+    // if (api_key !== process.env.API_KEY) return NextResponse.json({ title: "NOT MATCH API KEY", api_key });
+    // if (!url) return NextResponse.json({ title: "NO URL" });
 
-    console.log(api_key);
-    if (api_key !== process.env.API_KEY) return NextResponse.json({ title: "NOT MATCH API KEY", api_key });
-    if (!url) return NextResponse.json({ title: "NO URL" });
-
-    const data = await getOg(encodeURI(url));
+    // const data = await getOg(encodeURI(url));
     return NextResponse.json(data);
 }
 
