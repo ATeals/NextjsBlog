c77444625c5ead808a1e939105a37d1ae2ac676d
ateals <ateals@icloud.com>
2023-08-04 20:46:40 +0900

    delete header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 suppressHydrationWarning={true}
                 className=" dark:bg-[#1E1E1E] dark:text-[#e0e4e6] w-screen min-h-screen relative flex flex-col"
             >
-                <header className="shadowBottom mb-[10px] w-full bg-[#ffffff] dark:bg-darkBg z-10 fixed md:relative">
+                {/* <header className="shadowBottom mb-[10px] w-full bg-[#ffffff] dark:bg-darkBg z-10 fixed md:relative">
                     <Nav />
-                </header>
+                </header> */}
 
                 <main className="mb-[150px] lg:flex box-border w-full mt-[70px] md:mt-0">
                     <section className="w-full lg:w-[78%] box-border order-2">{children}</section>
